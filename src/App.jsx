0cb1e009fdf714d41b92806408c2f9eb8e551d72
Ahmad Ali <ahmad.ali619x@gmail.com>
2026-06-13 01:24:24 +0500

    Completed the Model rendering and product view page

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import ProductViewer from './components/ProductViewer'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/all'
 
 const App = () => {
+  gsap.registerPlugin(ScrollTrigger)
+
   return (
     <main>
       <Navbar />
--- src/components/ProductViewer.jsx
+++ src/components/ProductViewer.jsx
@@ -6,16 +6,20 @@ import { Box, OrbitControls } from '@react-three/drei';
 import MacbookModel14 from './models/Macbook-14';
 import { AmbientLight } from 'three';
 import StudioLights from './three/StudioLights';
+import ModelSwitcher from './three/ModelSwitcher'
+import { useMediaQuery } from 'react-responsive';
 
 const ProductViewer = () => {
   const { color, scale, setColor, setScale } = useMacbookStore();
+
+  const isMobile = useMediaQuery({ query: 'max-width: 1024px' })
 
   return (
     <section id='product-viewer'>
       <h2>Take a closer look.</h2>
 
       <div className="controls">
-        <p className="info">Macbook Pro {scale} in {color}</p>
+        {/* <p className="info">Macbook Pro | Available in 14" & 16" in Space Gray & Dark colors</p> */}
 
         <div className="flex-center gap-5 mt-5">
           <div className="color-control">
@@ -45,10 +49,9 @@ const ProductViewer = () => {
       <Canvas id='canvas' camera={{ position: [0, 2, 5], fov: 50, near: 0.1, far: 100 }}>
         <StudioLights />
 
-        <MacbookModel14 scale={0.06} position={[0, 0, 0]} />
         {/* In any three-dimensional space, the position is defined by three coordinates: x, y, and z. The x-coordinate represents the horizontal position, the y-coordinate represents the vertical position, and the z-coordinate represents the depth or distance from the viewer. In this case, the box is positioned at -1 on the x-axis (to the left), 1 on the y-axis (upwards), and 0 on the z-axis (at the same depth as the viewer). */}
 
-        <OrbitControls enableZoom={false} />
+        <ModelSwitcher scale={isMobile ? scale - 0.03 : scale} isMobile={isMobile} />
       </Canvas>
     </section>
   )
